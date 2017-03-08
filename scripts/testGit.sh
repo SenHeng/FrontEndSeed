a08ccec2c7f0ecab537b3a16dc209890204165b1
Sen Heng <sen@legendofcode.com>
2017-03-09 00:57:03 +0900

    fix: switch to https repo

--- scripts/testGit.sh
+++ scripts/testGit.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -ev # Exit with nonzero exit code if anything fails
 
-echo "token: ${GITHUB_TOKEN}"
+echo "cloning"
+git clone https://github.com/SenHeng/FrontEndSeed.git -b gh-pages dist
 
-git clone git@github.com:SenHeng/FrontEndSeed.git -b gh-pages dist
+echo "build"
 yarn run build
 cd dist
 
 git config user.name "Travis CI"
 git config user.email "sen@legendofcode.com"
 
+echo "git add"
 git add .
+echo "git commit"
 git commit -m "test: travis gh deploy"
+echo "git push"
 git push "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git"
